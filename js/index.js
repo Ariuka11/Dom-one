8e43236d774c13285143e163ed72a2eabd6cad7f
Ariunbold <ariun1118@gmail.com>
2019-08-07 20:57:53 -0400

    MVP done

--- js/index.js
+++ js/index.js
@@ -39,4 +39,81 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// Importing Images
+let ctaImg = document.querySelector('#cta-img');
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+let middleImg = document.querySelector('#middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);  
+
+//Importing Nav
+
+const anchors = document.querySelectorAll('a')
+
+// Array.from(anchors).forEach((anchor, index) => {
+//    return anchor[index].textContent = siteContent['nav'][`nav-item-1`]})
+
+anchors[0].textContent= siteContent['nav']['nav-item-1'];
+anchors[1].textContent= siteContent['nav']['nav-item-2'];
+anchors[2].textContent= siteContent['nav']['nav-item-3'];
+anchors[3].textContent= siteContent['nav']['nav-item-4'];
+anchors[4].textContent= siteContent['nav']['nav-item-5'];
+anchors[5].textContent= siteContent['nav']['nav-item-6'];
+
+//importing 1st section
+
+const h1 = document.querySelector('h1');
+const btn1 = document.querySelector('.cta button')
+
+h1.textContent = siteContent['cta']['h1'];
+btn1.textContent = siteContent['cta']['button'];
+
+//importing 2nd section
+const h4 = document.querySelectorAll('h4');
+const par = document.querySelectorAll('p');
+
+h4[0].textContent= siteContent[ "main-content"]["features-h4"];
+h4[1].textContent= siteContent[ "main-content"]["about-h4"];
+h4[2].textContent= siteContent[ "main-content"]["services-h4"];
+h4[3].textContent= siteContent[ "main-content"]["product-h4"];
+h4[4].textContent= siteContent[ "main-content"]["vision-h4"];
+
+par[0].textContent = siteContent["main-content"]["features-content"];
+par[1].textContent = siteContent["main-content"]["about-content"];
+par[2].textContent = siteContent["main-content"]["services-content"];
+par[3].textContent = siteContent["main-content"]["product-content"];
+par[4].textContent = siteContent["main-content"]["vision-content"];
+
+//importing last section
+
+h4[5].textContent= siteContent[ "contact"]["contact-h4"];
+par[5].textContent = siteContent['contact']['address'];
+par[6].textContent = siteContent['contact']['phone'];
+par[7].textContent = siteContent['contact']['email'];
+par[8].textContent = siteContent['footer']['copyright'];
+
+//Add new content
+
+const newAnchor1 = document.createElement('a');
+const newAnchor2 = document.createElement('a');
+
+newAnchor1.textContent = 'Sign Up';
+newAnchor2.textContent = 'Login';
+
+document.querySelector('nav').appendChild(newAnchor1);
+document.querySelector('nav').prepend(newAnchor2);
+
+//Changing nav color
+
+Array.from(document.querySelectorAll('a')).forEach(anchor => anchor.style.color = "PeachPuff");
+
+document.querySelector('body').style.background = 'linear-gradient(to right, #ff9966, #ff5e62)'
+document.querySelector('header').style.background = 'linear-gradient(to right, #0f2027, #203a43, #2c5364)'
+
+document.querySelector('.cta button').style.borderRadius = '12px'
+document.querySelector('.cta button').style.background = 'linear-gradient(to right, #12c2e9, #c471ed, #f64f59)'
+document.querySelector('.cta button').style.boxShadow = '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)'
+
+
